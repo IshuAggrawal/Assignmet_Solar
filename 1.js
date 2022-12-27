a1b27cdc0138eebdf0f969027787587521973157
IshuAggrawal <ishuaggrawal6789@gmail.com>
2022-12-27 13:21:00 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,28 @@
+function f1()
+{
+    var targetDiv=document.getElementById("v1");
+    if (targetDiv.style.visibility !== "visible") {
+        targetDiv.style.visibility = "visible";
+      } else {
+        targetDiv.style.visibility = "hidden";
+      }
+      
+}
+function f2()
+{
+    var targetDiv=document.getElementById("v2");
+    if (targetDiv.style.visibility !== "visible") {
+        targetDiv.style.visibility = "visible";
+      } else {
+        targetDiv.style.visibility = "hidden";
+      }
+}
+function f3()
+{
+    var targetDiv=document.getElementById("v3");
+    if (targetDiv.style.visibility !== "visible") {
+        targetDiv.style.visibility = "visible";
+      } else {
+        targetDiv.style.visibility = "hidden";
+      }
+}
